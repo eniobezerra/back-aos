332ef09d21b013b348e2aefa856c41008e8a8b02
eniobezerra <142815890+eniobezerra@users.noreply.github.com>
2025-11-17 18:40:13 +0000

    terminando atividade

--- api/service/userService.js
+++ api/service/userService.js
@@ -1,24 +1,31 @@
-import { Usuario } from '../models/index.js';
+import { Usuario, Endereco, Experiencia, Habilidade } from '../models/index.js';
 
 export const usuarioService = {
   criar: (dados) => Usuario.create(dados),
   listar: () => Usuario.findAll(),
   buscarPorId: (id) => Usuario.findByPk(id),
-  atualizar: (id, dados) => Usuario.update(dados, { where: { id_usuario: id } }),
-  deletar: (id) => Usuario.destroy({ where: { id_usuario: id } }),
-  buscarEndereco: (id) => 
+
+  atualizar: (id, dados) =>
+    Usuario.update(dados, { where: { id_usuario: id } }),
+
+  deletar: (id) =>
+    Usuario.destroy({ where: { id_usuario: id } }),
+
+  buscarEndereco: (id) =>
     Usuario.findByPk(id, {
       include: [{ model: Endereco, as: 'endereco' }]
     }),
 
-  buscarExperiencias: (id) => 
+  buscarExperiencias: (id) =>
     Usuario.findByPk(id, {
       include: [{ model: Experiencia, as: 'experiencias' }]
     }),
-  buscarHabilidades: (id) => 
+
+  buscarHabilidades: (id) =>
     Usuario.findByPk(id, {
       include: [{ model: Habilidade, as: 'habilidades' }]
     }),
+
   buscarCompleto: (id) =>
     Usuario.findByPk(id, {
       include: [
